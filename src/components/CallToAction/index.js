bca5693ea9ede8daf3426613d7a001503f67c635
toinelin <antoine.lin@hetic.net>
2017-10-03 19:32:30 +0200

    update

--- src/components/CallToAction/index.js
+++ src/components/CallToAction/index.js
@@ -6,7 +6,7 @@ import React from 'react'
 import styles from './styles.scss'
 
 const CallToAction = ({ link, content, color }: String) => (
-  <a href={link} target="_blank" className={`${styles.CallToAction} ${color}`} >
+  <a href={link} target="_blank" rel="noreferrer noopener" className={`${styles.CallToAction} ${color}`} >
     { content }
   </a>
 )
